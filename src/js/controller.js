5ce9a47fc70ee1ee6bbe075a5e086173f7d4ccf2
Scorpio1x1 <paulpavlovic07@gmail.com>
2024-07-22 19:18:48 +0200

    Updated for too small of a width

--- src/js/controller.js
+++ src/js/controller.js
@@ -147,11 +147,6 @@ const controlAddRecipe = async function (newRecipe) {
   // Upload new recipe data
 }
 
-const width = window.innerWidth;
-if (width < 660) {
-  window.alert('Your screen width is too small! Rotate screen');
-}
-
 // initialization function with their handlers 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -164,6 +159,15 @@ const init = function () {
 }
 init();
 
+
+const mes = document.querySelector('.recipe');
+const f = `<p class="width-size">Rotate Screen as it is too narrow for this application</p>`;
+
+const width = window.innerWidth;
+if (width < 660) {
+  window.alert('Your screen width is too small! Rotate screen!');
+  mes.insertAdjacentHTML('beforeend', f);
+}
 
 
 
